4a2c6d409224e76cc5f24b18c4752c1d9ca71f45
Carlos Garcia Velasco <charlitos_maquero@mac.com>
2018-10-02 16:33:48 -0400

    Using new project structure.

--- pretty_backtrace.rb
+++
@@ -1,8 +0,0 @@
-# Original author: Kristopher Johnson
-# https://gist.github.com/kristopherjohnson/e650487a5dc0ce9dd24ea2584db63e18
-
-# Generate a backtrace string for given exception.
-# Generated string is a series of lines, each beginning with a tab and "at ".
-def pretty_backtrace(exception)
-  "\tat #{exception.backtrace.join("\n\tat ")}"
-end
